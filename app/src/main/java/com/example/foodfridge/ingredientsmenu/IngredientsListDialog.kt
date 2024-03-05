7aad17a6ba46ca02dc847ae805159ae28978dc8a
aratakagan <aratakagan@gmail.com>
2024-03-05 00:02:35 -0800

    FOOD-93 add chip

--- app/src/main/java/com/example/foodfridge/ingredientsmenu/IngredientsListDialog.kt
+++ app/src/main/java/com/example/foodfridge/ingredientsmenu/IngredientsListDialog.kt
@@ -1,32 +1,24 @@
 package com.example.foodfridge.ingredientsmenu
 
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SuggestionChip
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
-import com.example.foodfridge.common.NormalTextBold
-import com.example.foodfridge.common.PantryCategory
 import com.example.foodfridge.common.PantryIngredient
 
+@OptIn(ExperimentalLayoutApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun IngredientsListDialog(
     onDismiss: ()->Unit,
@@ -46,22 +38,25 @@ fun IngredientsListDialog(
             modifier = Modifier
                         .fillMaxWidth(0.95f)
         ){
-            Column(
+            FlowRow(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(15.dp),
-                verticalArrangement = Arrangement.spacedBy(25.dp)
-            ) {
-                LazyRow{
-                    items(ingredients.size){index ->
-                        NormalTextBold(value = ingredients[index].ingredient_name, fontSize = 13, modifier = Modifier)
-                    }
-                }
+            ){
 
+                for (ingredient in ingredients){
+                    // NormalTextBold(value = ingredient.ingredient_name, fontSize = 13, modifier = Modifier)
+                    SuggestionChip(
+                        onClick = {},
+                        label = { Text(ingredient.ingredient_name)}
+                    )
+                }
+                }
             }
         }
     }
-}
+
+
 
 @Preview
 @Composable
